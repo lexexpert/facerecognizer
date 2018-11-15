34016da698df1bc580a1c48457ae073598fa476f
Ivan <ivanreshetar@gmail.com>
2018-11-14 18:59:41 -0600

    update register

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/register', {
+    fetch('https://facerecognizerapp-api.herokuapp.com:3000/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
